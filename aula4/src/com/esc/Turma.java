24d8cd26cd874a7d5e5d057fc2e5a5252915ceb3
sr.bruning <diego.brg92@gmail.com>
2023-08-04 16:57:48 -0300

    aluno arraylist

--- aula4/src/com/esc/Turma.java
+++ aula4/src/com/esc/Turma.java
@@ -1,15 +1,16 @@
 package com.esc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Turma {
-	
+
 	private String nome;
-	private Aluno[] alunos;
-	
-	private int qtdAlunos;
-	
+	private ArrayList<Aluno> alunos;
+
 	public Turma(String nome, int qtdAlunos) {
 		this.nome = nome;
-		alunos = new Aluno[qtdAlunos];
+		alunos = new ArrayList<Aluno>();
 	}
 
 	public String getNome() {
@@ -20,52 +21,48 @@ public class Turma {
 		this.nome = nome;
 	}
 
-	public Aluno[] getAlunos() {
-		
-		Aluno aux[] = new Aluno[qtdAlunos];
-		for(int i = 0; i< qtdAlunos; i++) {
-			aux[i] = alunos[i];
-		}
-		
+	public List<Aluno> getAlunos() {
+
+		ArrayList<Aluno> aux = new ArrayList<Aluno>();
+		aux.addAll(alunos);
+
 		return aux;
 	}
 
-	public void setAlunos(Aluno[] alunos) {
+	public void setAlunos(ArrayList<Aluno> alunos) {
 		this.alunos = alunos;
 	}
-	
+
 	/**
 	 * adiciona um novo aluno no ﬁnal
+	 * 
 	 * @param aluno
 	 */
 	public void adicionaAluno(Aluno aluno) {
-		
-		if(qtdAlunos>= alunos.length) {
-			return;
-		}
-		
-		alunos[qtdAlunos++] = aluno;
+
+		alunos.add(aluno);
 	}
-	
+
 	/**
 	 * calcula a média da turma com base na média dos alunos.
+	 * 
 	 * @return
 	 */
 	public float media() {
-		
+
 		float media = 0;
-		for(int i = 0; i< qtdAlunos; i++) {
-			Aluno aluno = alunos[i];
-			if(aluno==null)
+		int qtd = 0;
+		for (Aluno aluno : alunos) {
+			if (aluno == null)
 				continue;
-			
+			qtd++;
 			float mdAluno = aluno.media();
 			media += mdAluno;
 		}
-		
-		media = media / qtdAlunos;
-		
+
+		media = media / qtd;
+
 		return media;
 	}
-	
+
 }
